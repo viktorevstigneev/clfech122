a467167e67ba8bb2dd42f1bb6ddd63a811d8f4bf
surprise <viktar.yeustsihneyeu@instinctools.com>
2023-06-18 04:42:11 +0300

    я инициализо=ировал проект и создал шаблон html

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: 'http://localhost:3000',
+		origin: 'https://f1-opef.onrender.com',
 		preflightContinue: true,
 		optionsSuccessStatus: 200,
 		credentials: true,
